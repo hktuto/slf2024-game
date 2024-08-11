1463c80138c46b01ec7f161ae814479888b9669b
Sean Tsang <9394wing@gmail.com>
2024-08-11 15:00:35 +0800

    add empty page

---
+++ pages/empty.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+
+const {loadQuestion } = useGame();
+
+
+</script>
+
+<template>
+    <div class="pageContainer">
+    </div>
+</template>
+
+<style scoped>
+.pageContainer{
+    width:100%;
+    height: 100%;
+    display: grid;
+    place-items: center;
+}
+.center{
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 24px;
+}
+.title_logo{
+    width:100%;
+}
+button{
+    border:0;
+    padding: 12px 24px;
+    text-align: center;
+    background: #6bee85;
+    border: 1px solid #fff;
+    border-radius: 12px;
+    font-size: 1.2rem;
+    font-weight: 900;
+    cursor: pointer;
+}
+h1{
+    margin:0;
+    font-size: 2.5rem;;
+}
+</style>
